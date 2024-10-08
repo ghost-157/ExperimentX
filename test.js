0eb10b9b73d8dc9b8a2b844c017791b11f742c0e
manohar.m <manohar.m@toasttab.com>
2024-10-08 12:40:14 +0530

    updated the comment

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const dPattern = require('./PatternX')
 const debug1 = require('./debug')
 
+//for testing purpose we are creating the instance for the patternX
 const pattern2 = new dPattern(debug1)
 
 module.exports = pattern2
